1053d53d42c6edd8b7d907057497c7b25b449ea8
JJGB2311 <jgamezb@miumg.edu.gt>
2021-05-25 18:00:42 -0600

    Manejos de estados

--- src/Components/Badge.js
+++ src/Components/Badge.js
@@ -1,11 +1,9 @@
-import React from "react";
-import confLogo from "../Image/badge-header.svg";
+import React from 'react';
+import '../Components/style/Badge.css';
+import confLogo from '../Image/badge-header.svg';
+
 import foto from "../Image/CV.jpg";
-import "./style/Badge.css";
-const primern='Juan Jose'
-const segudon ='Gamez Blanco'
-const puesto = 'Frontend Engineer'
-const correo = 'Jgamezb'
+
 
 class Badge extends React.Component {
   render() {
@@ -16,15 +14,19 @@ class Badge extends React.Component {
         </div>
 
         <div className="Badge__section-name">
-          <img className="Badge__avatar" src={foto} alt="Avatar" />
+          <img
+            className="Badge__avatar"
+            src={foto}
+            alt="Avatar"
+          />
           <h1>
-            {this.props.primern} <br /> {this.props.segudon}
+            {this.props.firstName} <br /> {this.props.lastName}
           </h1>
         </div>
 
         <div className="Badge__section-info">
-          <h3>{puesto}</h3>
-          <div>@{this.props.correo}</div>
+          <h3>{this.props.jobTitle}</h3>
+          <div>@{this.props.twitter}</div>
         </div>
 
         <div className="Badge__footer">#platziconf</div>
--- src/Pages/BadgeNew.js
+++ src/Pages/BadgeNew.js
@@ -1,33 +1,54 @@
-import React from 'react';
-import Navbar from '../Components/Navbar'
-import IMG from '../Image/badge-header.svg'
-import  '../Pages/styles/BadgeNew.css'
-import Badge from '../Components/Badge'
-class BadgeNew extends React.Component{
-    render(){
-return(
-
-    <div>
-<Navbar/>
-<div className="BadgeNew__hero">
-<img className="img_fluid" src={IMG}/>
-</div>
-<div className="Container">
-    .
-</div>    
-<div className="row">
-    <div className="col">
-        <Badge  primern='Juan Jose'
-segudon ='Gamez Blanco'
-puesto = 'Frontend Engineer'
- correo = 'Jgamezb'/>
-    </div>
-</div>
-    </div>
-)
-    
-
-
-    }
+import React from "react";
+import IMG from "../Image/badge-header.svg";
+import "../Pages/styles/BadgeNew.css";
+import BadgeForm from "../Components/BadgeForm";
+import Badge from "../Components/Badge";
+class BadgeNew extends React.Component {
+    state = {
+        form: {
+          firstName: '',
+          lastName: '',
+          email: '',
+          jobTitle: '',
+          twitter: '',
+        },
+      };
+      handleChange = e => {
+        this.setState({
+          form: {
+            ...this.state.form,
+            [e.target.name]: e.target.value,
+          },
+        });
+      };
+  render() {
+    return (
+      <div>
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={IMG} />
+        </div>
+      
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Badge
+               firstName={this.state.form.firstName}
+               lastName={this.state.form.lastName}
+               twitter={this.state.form.twitter}
+               jobTitle={this.state.form.jobTitle}
+               email={this.state.form.email}
+              />
+            </div>
+            <div className="col-6">
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-export  default BadgeNew;
+export default BadgeNew;
